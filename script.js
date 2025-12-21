26f32716f802a24b949040a255e36e9619ef5b6c
7sn-al <kolkaka40@gmail.com>
2025-12-21 18:56:36 +0200

    shabeb  new commit la shlek tourist

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 const btn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".mobile_links");
+const logo =document.querySelector(".logo");
+const logoImg=document.querySelector(".logo-image");
 
 btn.addEventListener("click", () => {
     menu.classList.toggle("open");
+
+    if (menu.classList.contains('open')) {
+        btn.innerHTML = '✕';
+    } else {
+        btn.innerHTML = '☰';
+    }
 });
+
+let propertiesAfter=(e) =>{
+    e.style.backgroundColor = "transparent";
+    e.style.boxShadow = "none";
+    e.style.backdropFilter = "none";
+}
+
+let propertiesBefore=(e) =>{
+    e.style.backgroundColor = "rgba(0, 0, 0, 0.4)"; 
+    e.style.boxShadow = "0 4px 12px rgba(0, 0, 0,0.3)";
+    e.style.backdropFilter="blur(5px)";
+}
+
+
+const header = document.querySelector(".logodiv");
+
+    window.addEventListener('scroll', function() {
+    if (window.scrollY > 70) { 
+
+        header.classList.add('scrolled');
+
+        propertiesAfter(btn);
+        propertiesAfter(logo);
+        propertiesAfter(logoImg);
+
+
+    } else {
+
+        header.classList.remove('scrolled');
+
+        propertiesBefore(btn);
+        propertiesBefore(logo);
+        propertiesBefore(logoImg);
+    }
+});
+
